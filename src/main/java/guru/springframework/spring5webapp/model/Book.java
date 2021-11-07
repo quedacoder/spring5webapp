48b78c0f799a8a67240439b497e81ed3e13e2dd4
quedacoder <quedacoder@gmail.com>
2021-11-07 14:25:57 -0600

    add thymeleaf template to display a list of authors

--- src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -106,7 +106,7 @@ public class Book {
 
 	@Override
 	public String toString() {
-		return "Book [id=" + id + ", title=" + title + ", isbn=" + isbn + ", authors=" + authors + ", publisher="
+		return "Book [id=" + id + ", title=" + title + ", isbn=" + isbn + ", publisher="
 				+ publisher + "]";
 	}
 	
--- src/main/java/guru/springframework/spring5webapp/model/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/model/Publisher.java
@@ -121,6 +121,6 @@ public class Publisher {
 	@Override
 	public String toString() {
 		return "Publisher [id=" + id + ", name=" + name + ", addressLine1=" + addressLine1 + ", city=" + city
-				+ ", state=" + state + ", zip=" + zip + ", books=" + books + "]";
+				+ ", state=" + state + ", zip=" + zip + "]";
 	}
 }
